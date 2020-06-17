c98b2d75583ad72e96d4e82d930e241f5382f90b
AdomasZi <adam.zitkus@student.nhlstenden.com>
2020-06-17 14:47:27 +0300

    Reset password done

--- app/src/main/java/com/annikadietz/shoppy_shoppingbuddy/ui/login/LoginFragment.kt
+++ app/src/main/java/com/annikadietz/shoppy_shoppingbuddy/ui/login/LoginFragment.kt
@@ -1,21 +1,27 @@
 package com.annikadietz.shoppy_shoppingbuddy.ui.login
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
 import android.text.TextUtils
 import android.util.Log
+import android.util.Patterns
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.annikadietz.shoppy_shoppingbuddy.MainActivity
 import com.annikadietz.shoppy_shoppingbuddy.NewDatabaseHelper
 import com.annikadietz.shoppy_shoppingbuddy.R
-import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
@@ -23,7 +29,7 @@ import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class LoginFragment : Fragment() {
-    private var mAuth: FirebaseAuth? = null
+    private var mAuth: FirebaseAuth? = FirebaseAuth.getInstance()
 
     private lateinit var loginViewModel: LoginViewModel
 
@@ -40,8 +46,10 @@ class LoginFragment : Fragment() {
         addProductButton.setOnClickListener {
             loginButtonClicked()
         }
-
-        mAuth = FirebaseAuth.getInstance()
+        val resetPasswordButton: TextView = root.findViewById(R.id.reset_btn)
+        resetPasswordButton.setOnClickListener {
+            resetPassword()
+        }
 
         if (mAuth!!.currentUser != null) {
 //            startActivity(Intent(this, MainActivity::class.java))
@@ -49,6 +57,48 @@ class LoginFragment : Fragment() {
         }
 
         return root
+    }
+
+    private fun resetPassword() {
+        val builder: android.app.AlertDialog.Builder = android.app.AlertDialog.Builder(this.context)
+
+        val title = TextView(this.context)
+        title.text = "Enter your email"
+        title.setPadding(10, 70, 10, 70)
+        title.gravity = Gravity.CENTER
+        title.textSize = 20f
+        builder.setCustomTitle(title)
+        val input = EditText(this.context)
+
+        input.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+        builder.setView(input)
+
+        builder.setPositiveButton("Reset password"
+        ) { dialog, which ->
+            var email = input.text.toString()
+            mAuth?.sendPasswordResetEmail(email)?.addOnCompleteListener{
+                if (it.isSuccessful){
+                    Toast.makeText(activity, "Success!", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(activity, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        builder.setNegativeButton("Cancel",
+            DialogInterface.OnClickListener { dialog, which -> dialog.cancel() })
+
+        val dialog: AlertDialog = builder.create()
+
+        input.doOnTextChanged { text, start, count, after ->
+            if(Patterns.EMAIL_ADDRESS.matcher(text).matches()){
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = true
+            } else {
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = false
+            }
+        }
+        dialog.show()
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = false
     }
 
     private fun loginButtonClicked() {
