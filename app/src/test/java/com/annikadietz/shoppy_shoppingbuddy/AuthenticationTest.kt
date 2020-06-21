e869657b2acc482cd4a57133fb77ffb2a79f0341
ramimazloum871 <37105837+ramimazloum871@users.noreply.github.com>
2020-06-21 13:33:09 +0200

    Create dummy data , make 3 function to test comination

--- app/src/test/java/com/annikadietz/shoppy_shoppingbuddy/AuthenticationTest.kt
+++ app/src/test/java/com/annikadietz/shoppy_shoppingbuddy/AuthenticationTest.kt
@@ -2,12 +2,18 @@ package com.annikadietz.shoppy_shoppingbuddy
 
 import android.content.Context
 import android.widget.EditText
+import androidx.fragment.app.Fragment
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.viewpager2.widget.ViewPager2
+import com.annikadietz.shoppy_shoppingbuddy.NewDatabaseHelper.uid
 import com.annikadietz.shoppy_shoppingbuddy.ui.login.LoginFragment
 import com.annikadietz.shoppy_shoppingbuddy.ui.register.RegisterFragment
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.OnFailureListener
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.AuthResult
+import kotlinx.android.synthetic.main.activity_auth.*
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
@@ -15,15 +21,17 @@ import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
-
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [Config.OLDEST_SDK])
 class AuthenticationTest {
     private lateinit var instrumentationContext: Context
 
+
     @Before
     fun setup() {
+
         instrumentationContext = InstrumentationRegistry.getInstrumentation().context
+        FirebaseApp.initializeApp(instrumentationContext);
     }
 
     @Test
@@ -32,9 +40,11 @@ class AuthenticationTest {
         email.setText("stephantest@ruiz.com")
         val password = EditText(instrumentationContext)
         password.setText("stephan")
-        val registerFrag = RegisterFragment()
+       var instrumentationContext2 = InstrumentationRegistry.getInstrumentation().context
 
-
+//Todo it's not working at this moment
+        val viewPager=ViewPager2(instrumentationContext2)
+       val registerFrag = RegisterFragment(viewPager)
         val result = registerFrag.registerUser(email, password)
             ?.addOnCompleteListener(OnCompleteListener<AuthResult?> { task ->
                 val str = email.text.toString()
@@ -45,7 +55,6 @@ class AuthenticationTest {
                 } else {
                     println(task.exception.toString())
                     Assert.assertEquals(task.result, "")
-
                 }
             })?.addOnFailureListener(OnFailureListener { task ->
                 task.message
@@ -61,6 +70,7 @@ class AuthenticationTest {
         val password = EditText(instrumentationContext)
         password.setText("stephan")
         val loginFrag = LoginFragment()
+
 //        loginFrag.progressBar.progress = findViewById(R.id.progressBar)
 
         val result = loginFrag.loginUser(email, password)
@@ -77,4 +87,5 @@ class AuthenticationTest {
             })
         println()
     }
+
 }
